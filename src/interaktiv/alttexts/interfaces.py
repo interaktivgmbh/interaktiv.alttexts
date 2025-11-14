f4cdbf7096d7cc15e470102da8bf691051786061
Alexander Rybakov <rybakov@interaktiv.de>
2025-11-14 15:53:07 +0100

    show image alt text in image view

--- src/interaktiv/alttexts/interfaces.py
+++ src/interaktiv/alttexts/interfaces.py
@@ -1,7 +1,8 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.app.contenttypes.interfaces import IPloneAppContenttypesLayer
 
 
-class IBrowserLayer(IDefaultBrowserLayer):
+class IInteraktivAltTextBrowserLayer(IPloneAppContenttypesLayer, IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
