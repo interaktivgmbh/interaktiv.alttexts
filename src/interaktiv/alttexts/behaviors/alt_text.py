f29c3b784fd8f24ecc5281f595d769c6876ed8b2
Alexander Rybakov <rybakov@interaktiv.de>
2025-11-14 16:04:47 +0100

    fix linting

--- src/interaktiv/alttexts/behaviors/alt_text.py
+++ src/interaktiv/alttexts/behaviors/alt_text.py
@@ -1,10 +1,8 @@
 from interaktiv.alttexts import _
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
 from zope import schema
 from zope.interface import Interface
-from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import provider
 
--- src/interaktiv/alttexts/indexers/alt_text.py
+++ src/interaktiv/alttexts/indexers/alt_text.py
@@ -4,4 +4,4 @@ from plone.indexer import indexer
 
 @indexer(IImage)
 def alt_text_indexer(obj):
-    return getattr(obj, 'alt_text', '')
+    return getattr(obj, "alt_text", "")
