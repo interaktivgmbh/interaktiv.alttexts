e97432746ef20e8878ecba293e7a8efd8b9777df
Alexander Rybakov <rybakov@interaktiv.de>
2025-11-18 16:18:38 +0100

    fix formatting

--- src/interaktiv/alttexts/behaviors/alt_text.py
+++ src/interaktiv/alttexts/behaviors/alt_text.py
@@ -13,10 +13,12 @@ class IAltTextBehavior(model.Schema):
         title=_("alt_text_label", default="Alt text"),
         description=_(
             "alt_text_description",
-            default=("An alternative text makes the image accessible for "
-                     "people using assistive technologies and as fallback for "
-                     "when the image fails to load. Decorative images should "
-                     "not have an alternative text."),
+            default=(
+                "An alternative text makes the image accessible for "
+                "people using assistive technologies and as fallback for "
+                "when the image fails to load. Decorative images should "
+                "not have an alternative text."
+            ),
         ),
         required=False,
         default="",
